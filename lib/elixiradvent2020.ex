cb2f2616b31286ebe317cdd3757d5a9d68f39cff
stsullivan <stephen.sullivan@priceline.com>
2021-11-18 17:01:23 -0500

    Initial empty commit

---
+++ lib/elixiradvent2020.ex
@@ -0,0 +1,18 @@
+defmodule Elixiradvent2020 do
+  @moduledoc """
+  Documentation for `Elixiradvent2020`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Elixiradvent2020.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/elixiradvent2020_test.exs
@@ -0,0 +1,8 @@
+defmodule Elixiradvent2020Test do
+  use ExUnit.Case
+  doctest Elixiradvent2020
+
+  test "greets the world" do
+    assert Elixiradvent2020.hello() == :world
+  end
+end
