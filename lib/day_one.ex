844b38083d32d8cc3d6a7b41d4ec274a5c3629d4
stsullivan <stephen.sullivan@priceline.com>
2021-11-19 15:35:03 -0500

    1. Completed dayOne puzzle two. refactoring a little.

--- lib/day_one.ex
+++ lib/day_one.ex
@@ -1,26 +1,31 @@
 defmodule DayOne do
   @moduledoc false
 
-  def is2020(x,y) do
+  def is_target(target, x,y) do
     case (x+y) do
-      2020 -> [x,y]
+      ^target -> [x,y]
       _ -> :error
     end
   end
 
-  def find_2020([x,y]), do: is2020(x,y)
-  def find_2020([x|tl]) do
-    case find_2020(x,tl) do
+  def find_2020(list) do
+    find_target(2020, list)
+  end
+
+
+  def find_target(target, [x,y]), do: is_target(target, x,y)
+  def find_target(target, [x|tl]) do
+    case find_target(target, x,tl) do
       [x,y] -> [x,y]
-      :error -> find_2020(tl)
+      :error -> find_target(target, tl)
     end
   end
 
-  def find_2020(_,[]), do: :error
-  def find_2020(x,[y|tl]) do
-    case is2020(x,y) do
+  def find_target(_, _,[]), do: :error
+  def find_target(target, x,[y|tl]) do
+    case is_target(target, x,y) do
       [x,y] -> [x,y]
-      :error -> find_2020(x,tl)
+      :error -> find_target(target, x,tl)
     end
   end
 
@@ -33,8 +38,31 @@ defmodule DayOne do
 
   def calculate(file) do
     file
-    |> find_pair_from_file
-    |> Enum.product
+    |> find_product_from_file(&find_2020/1)
   end
+
+  def find_product_from_file(file, find_func) do
+    file
+    |> File.read!()
+    |> String.split()
+    |> Enum.map(&String.to_integer/1)
+    |> find_func.()
+    |> Enum.product()
+  end
+
+  def triplet_product_from_file(file) do
+    file
+    |> find_product_from_file(&find_triplet_2020/1)
+  end
+
+  def find_triplet_2020([x | tl]) do
+    diff = 2020-x
+    case find_target(diff, tl) do
+      :error -> find_triplet_2020(tl)
+      [y,z] -> [x,y,z]
+    end
+
+  end
+
 
 end
--- test/day_one_test.exs
+++ test/day_one_test.exs
@@ -3,8 +3,8 @@ defmodule DayOneTest do
   import DayOne
 
   test "two numbers 2020" do
-    assert is2020(0, 0) == :error
-    assert is2020(2015, 5) == [2015, 5]
+    assert is_target(2020, 0, 0) == :error
+    assert is_target(2020, 2015, 5) == [2015, 5]
   end
 
   test "find pair that equals 2020" do
@@ -16,6 +16,10 @@ defmodule DayOneTest do
     assert find_2020([0, 2019, 0, 1]) == [2019, 1]
   end
 
+  test "find pair that equals 2019" do
+    assert find_target(2019, [0, 2018, 0, 1]) == [2018, 1]
+  end
+
   test "find pair from file" do
     assert find_pair_from_file("priv/testinput.txt") == [1,2019]
   end
@@ -25,6 +29,24 @@ defmodule DayOneTest do
   end
 
   test "calculate from input" do
-    calculate("priv/dayoneinput.txt") |> IO.puts
+
+    IO.puts("\n pair product is #{calculate("priv/dayoneinput.txt")}\n")
   end
+
+  test "find triplet that is 2020" do
+    assert find_triplet_2020([2018, 1, 1]) == [2018, 1, 1]
+    assert find_triplet_2020([2018, 1, 1, 0]) == [2018, 1, 1]
+    assert find_triplet_2020([0, 2018, 1, 1]) == [2018, 1, 1]
+    assert find_triplet_2020([0, 2018, 1, 0, 1]) == [2018, 1, 1]
+  end
+
+  test "triplet_from_file" do
+    assert triplet_product_from_file("priv/testinput_3.txt") == 2018
+  end
+
+  test "calculate triplet_product_from_file input triplet" do
+    IO.puts("\n triplet_product_from_file is #{triplet_product_from_file("priv/dayoneinput.txt")}\n")
+  end
+
+
 end
