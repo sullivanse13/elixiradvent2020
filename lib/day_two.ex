64e89d1a80aede24faa31ffe8c257f3bf82312cb
stsullivan <stephen.sullivan@priceline.com>
2021-11-21 14:09:16 -0500

    1. finished day two puzzle two.

--- lib/day_two.ex
+++ lib/day_two.ex
@@ -2,36 +2,53 @@ defmodule DayTwo do
   @moduledoc false
 
 
-  def parse_line(line) do
+  def count_valid_passwords_position(file) do
+    file
+    |> File.read!()
+    |> String.split("\n")
+    |> Enum.map(fn line -> String.split(line, ~r/-|:? /) end)
+    |> Enum.count(&is_valid_positional/1)
+  end
+
+  def is_valid_positional([low, high, required, string]) do
+    [low,high]
+    |> Enum.map(&String.to_integer/1)
+    |> Enum.map(fn x -> x-1 end)
+    |> Enum.map(fn position -> String.at(string, position) end)
+    |> Enum.count(fn char -> char == required end)
+    |> Kernel.==(1)
+  end
+
+  def parse_line_range(line) do
     [range_start, range_end, letter, password] = String.split(line, ~r/-|:? /)
 
     {String.to_integer(range_start)..String.to_integer(range_end), letter, password}
   end
 
-  def count_required(required, password) do
+  def count_required_range(required, password) do
     password
     |> String.codepoints
     |> Enum.count(fn x -> x == required end)
   end
 
-  def is_valid_password({range, required, password}) do
+  def is_valid_password_range({range, required, password}) do
     range
-    |> Enum.member?(count_required(required, password))
+    |> Enum.member?(count_required_range(required, password))
   end
 
 
-  def count_valid_passwords(file) when is_binary(file) do
+  def count_valid_passwords_range(file) when is_binary(file) do
     file
     |> File.read!()
     |> String.split("\n")
-    |> count_valid_passwords()
+    |> count_valid_passwords_range()
   end
 
 
-  def count_valid_passwords(list) when is_list(list) do
+  def count_valid_passwords_range(list) when is_list(list) do
     list
-    |> Enum.map(&parse_line/1)
-    |> Enum.count(&is_valid_password/1)
+    |> Enum.map(&parse_line_range/1)
+    |> Enum.count(&is_valid_password_range/1)
   end
 
 end
--- test/day_two_test.exs
+++ test/day_two_test.exs
@@ -11,17 +11,18 @@ defmodule DayTwoTest do
   #
 
   test "parse line" do
-    assert parse_line("1-3 a: abcde") == {1..3, "a", "abcde"}
+    assert parse_line_range("1-3 a: abcde") == {1..3, "a", "abcde"}
   end
 
   test "count required" do
-    assert count_required("a", "abcde") == 1
-    assert count_required("c", "acbcdec") == 3
+    assert count_required_range("a", "abcde") == 1
+    assert count_required_range("c", "acbcdec") == 3
   end
 
   test "is valid password" do
-    assert is_valid_password({1..3, "a", "abcde"})
-    refute is_valid_password({1..3, "b", "cdefg"})
+    assert is_valid_password_range({1..3, "a", "abcde"})
+    refute is_valid_password_range({1..3, "b", "cdefg"})
+    assert is_valid_password_range({2..9, "c", "ccccccccc"})
   end
 
   test "count valid in list" do
@@ -29,15 +30,31 @@ defmodule DayTwoTest do
       "1-3 b: cdefg",
       "2-9 c: ccccccccc"]
 
-    assert count_valid_passwords(list) == 2
+    assert count_valid_passwords_range(list) == 2
   end
 
-  test "count from file" do
-    assert count_valid_passwords("priv/day_two_input_test.txt") == 2
+  test "count from file range" do
+    assert count_valid_passwords_range("priv/day_two_input_test.txt") == 2
   end
 
-  test "from input" do
-    IO.puts("\nvalid password count: #{count_valid_passwords("priv/day_two_input.txt")}\n")
+  test "from input count of valid by range" do
+    IO.puts("\nvalid password count range: #{count_valid_passwords_range("priv/day_two_input.txt")}\n")
+  end
+
+
+  test "count from file position" do
+    assert count_valid_passwords_position("priv/day_two_input_test.txt") == 1
+    assert count_valid_passwords_position("priv/day_two_input.txt") > 389
+  end
+
+  test "is valid positional" do
+    assert is_valid_positional(["1","3", "a", "abcde"])
+    refute is_valid_positional(["1","3", "b", "cdefg"])
+    refute is_valid_positional(["2","9", "c", "ccccccccc"])
+  end
+
+  test "from input count of valid by position" do
+    IO.puts("\nvalid password count position: #{count_valid_passwords_position("priv/day_two_input.txt")}\n")
   end
 
 
