490632fd7c4979a61d7759c9436f082bbc325c88
stsullivan <stephen.sullivan@priceline.com>
2021-11-26 10:31:14 -0500

    1. Completed day four puzzle two. Refactoring 'key not found' handling to with statement.

--- lib/day_four.ex
+++ lib/day_four.ex
@@ -9,8 +9,72 @@ defmodule DayFour do
   end
 
   def valid_passport?(passport) when is_map(passport) do
-    ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]
-    |> Enum.all?(fn required -> Map.has_key?(passport, required) end)
+    (has_expected_keys?(passport) and has_valid_expected_fields?(passport))
+  end
+
+  defp has_valid_expected_fields?(passport) do
+    [{"byr", &valid_byr?/1},
+        {"iyr", &valid_iyr?/1},
+        {"eyr", &valid_eyr?/1},
+        {"pid", &valid_pid?/1},
+        {"hcl", &valid_hcl?/1},
+        {"ecl", &valid_ecl?/1},
+        {"hgt", &valid_hgt?/1}
+      ]
+      |> Enum.map(fn {key, func} -> {Map.fetch(passport, key), func} end)
+      |> Enum.all?(fn {value, func} -> func.(value) end)
+
+  end
+
+  def run_validation({key, func}, passport) do
+    with {:ok, value} <- Map.fetch(passport, key) do
+      func.(value)
+    else
+      _ -> false
+    end
+  end
+
+
+  def valid_hgt?(:error), do: false
+  def valid_hgt?({:ok, hgt}) do
+    case Integer.parse(hgt) do
+      {_, ""} -> false
+      {inches, "in"} -> inches in 59..76
+      {cm, "cm"} -> cm in 150..193
+    end
+  end
+
+
+  def valid_byr?(byr), do: valid_number?(byr, 1920..2002)
+  def valid_iyr?(iyr), do: valid_number?(iyr, 2010..2020)
+  def valid_eyr?(eyr), do: valid_number?(eyr, 2020..2030)
+
+  def valid_ecl?(:error), do: false
+  def valid_ecl?({:ok, ecl}) do
+    ecl in ["amb","blu","brn","gry","grn","hzl","oth"]
+  end
+
+  def valid_hcl?(:error), do: false
+  def valid_hcl?({:ok, hcl}) do
+    Regex.match?(~r/#[0-9|a-f]{6}/, hcl)
+  end
+
+  def valid_pid?(:error), do: false
+  def valid_pid?({:ok, pid}) do
+    case Integer.parse(pid) do
+      {num, ""} -> String.length(pid) == 9 and num > 0
+      _ -> false
+    end
+  end
+
+  def valid_number?(:error, _), do: false
+  def valid_number?({:ok, string_year}, range) do
+    String.to_integer(string_year) in range
+  end
+
+  defp has_expected_keys?(passport) do
+    expected_keys = ["eyr", "hgt", "ecl", "pid"]
+    Enum.all?(expected_keys, fn required -> Map.has_key?(passport, required) end)
   end
 
   def read_passports(passports) do
--- test/day_four_test.exs
+++ test/day_four_test.exs
@@ -2,22 +2,7 @@ defmodule DayFourTest do
   use ExUnit.Case
   import DayFour
 
-  @docp """
 
-  [x] count valid passports in file
-  [x] CHECK FOR Required fields
-    byr (Birth Year)
-    iyr (Issue Year)
-    eyr (Expiration Year)
-    hgt (Height)
-    hcl (Hair Color)
-    ecl (Eye Color)
-    pid (Passport ID)
-    cid (Country ID)
-  [x] valid of only missing cid (north pole credential)
-  [x] Read passport
-  [x] Read multiple passports
-  """
 
   test "count valid passports in file" do
     assert count_valid_from_file("priv/day_four_input_test.txt") == 2
@@ -26,6 +11,14 @@ defmodule DayFourTest do
   test "count valid passports in input" do
     "valid passorts #{count_valid_from_file("priv/day_four_input.txt")}\n"
     |> IO.puts()
+  end
+
+  test "zero valid passports in test file" do
+    assert count_valid_from_file("priv/day_four_input_test_4_invlid_passwords.txt") == 0
+  end
+
+  test "4 valid passports in test file" do
+    assert count_valid_from_file("priv/day_four_input_test_4_valid_passwords.txt") == 4
   end
 
   @passport_one %{
@@ -48,6 +41,48 @@ defmodule DayFourTest do
     "iyr" => "2013",
     "cid" => "350"
   }
+
+  test "validate birth year", do: assert_field_validation("byr", ["2003", "1919"])
+  test "validate issue year", do: assert_field_validation("iyr", ["2009", "2021"])
+  test "validate expiration year", do: assert_field_validation("eyr", ["2019", "2031"])
+  test "validate passport id", do: assert_field_validation("pid", ["01234567", "0123456789"])
+  test "validate hair color", do: assert_field_validation("hcl", ["#123abz", "#123abz"])
+  test "validate eye color", do: assert_field_validation("ecl", ["zzz", "xyz"])
+  test "validate height color", do: assert_field_validation("hgt", ["149cm", "194cm","190", "58in", "77in"])
+
+  @docp """
+  [ ] stop having to handle not found error for everything?
+
+  [ ] validate height (cm, in)
+  [x] validate eye color
+  [x] validate hair color
+  [x] validate birth year
+  [x] validate issue year
+  [x] validate expiration year
+  [x] validate passport id
+  [x] count valid passports in file
+  [x] CHECK FOR Required fields
+    byr (Birth Year)
+    iyr (Issue Year)
+    eyr (Expiration Year)
+    hgt (Height)
+    hcl (Hair Color)
+    ecl (Eye Color)
+    pid (Passport ID)
+    cid (Country ID)
+  [x] valid of only missing cid (north pole credential)
+  [x] Read passport
+  [x] Read multiple passports
+  """
+
+  defp assert_field_validation(key, invalids) do
+    assert valid_passport?(@passport_one) == true
+    assert valid_passport?(Map.drop(@passport_one, [key])) == false, "should have failed with dropped key: #{key}"
+    invalids
+    |> Enum.map(fn i ->
+      assert valid_passport?(Map.put(@passport_one, key, i)) == false, "key: #{key} should fail with #{i}" end)
+  end
+
 
   test "validate passport" do
     assert valid_passport?(@passport_one) == true
